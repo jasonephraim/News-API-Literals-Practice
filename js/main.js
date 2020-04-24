ad4f3087192162ddc03d718a43d9825a23845003
Jason Ephraim <jason.ephraim.novatex@gmail.com>
2020-04-23 20:23:13 -0500

    init commit

---
+++ js/main.js
@@ -0,0 +1,25 @@
+// set newsList variable as html ul element id
+
+let newsList = document.getElementById("news");
+
+// set list of articles as articles in the news.js array mapped using literals
+
+let articlesList = news.articles.map(function (article) {
+  let articleInList = `<li class="list-group-item borderless">
+            <div class="card" style="width: 20rem;">
+                <div class="card-header">Published: ${article.publishedAt}</div>
+                <img class="card-img-top" src =${article.urlToImage} >
+                <div class="card-body">
+                    <h5 class="card-title">${article.title}</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">${article.description}</h6>
+                    <p class="card-text">${article.content}</p>
+                    <a href="${article.url}" class="btn btn-primary">Read Article</a>
+                </div>
+                </div>
+                </li>`;
+  return articleInList;
+});
+
+// populated html from articlesList
+
+newsList.innerHTML = articlesList.join(" ");
